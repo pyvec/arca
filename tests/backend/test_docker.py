07112427a0f9f35a6b49a79fddb65e38ee020362
Mikuláš Poul <mikulaspoul@gmail.com>
2017-12-01 17:32:37 +0100

    Testing specific python version

--- tests/backend/test_docker.py
+++ tests/backend/test_docker.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from uuid import uuid4
 import os
 
+import pytest
 from git import Repo
 
 from arca import Arca, DockerBackend, Task
@@ -10,6 +11,13 @@ from arca import Arca, DockerBackend, Task
 RETURN_STR_FUNCTION = """
 def return_str_function():
     return "Some string"
+"""
+
+RETURN_PYTHON_VERSION_FUNCTION = """
+import sys
+
+def return_python_version():
+    return "{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro)
 """
 
 
@@ -66,3 +74,42 @@ def test_keep_container_running():
     count_after_stop = len(backend.client.containers.list())
 
     assert count_after_stop == container_count
+
+
+@pytest.mark.parametrize("python_version", ["3.6.0", "3.6.3"])
+def test_python_version(python_version):
+    kwargs = {}
+
+    if os.environ.get("TRAVIS", False):
+        base_dir = "/home/travis/build/{}/test_loc".format(os.environ.get("TRAVIS_REPO_SLUG", "mikicz/arca"))
+    else:
+        base_dir = "/tmp/arca/test"
+
+    backend = DockerBackend(base_dir=base_dir, verbosity=2,
+                            python_version=python_version, **kwargs)
+
+    arca = Arca(backend=backend)
+
+    git_dir = Path("/tmp/arca/") / str(uuid4())
+
+    repo = Repo.init(git_dir)
+    filepath = git_dir / "test_file.py"
+
+    filepath.write_text(RETURN_PYTHON_VERSION_FUNCTION)
+    repo.index.add([str(filepath)])
+    repo.index.commit("Initial")
+
+    task = Task(
+        "return_python_version",
+        from_imports=[("test_file", "return_python_version")]
+    )
+
+    result = arca.run(f"file://{git_dir}", "master", task)
+
+    try:
+        print(result.error)
+    except AttributeError:
+        pass
+
+    assert result.success
+    assert result.result == python_version
