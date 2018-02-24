def76f30cf4dad046320b53dd6d10b787a9e8e7b
Mikuláš Poul <mikulaspoul@gmail.com>
2018-02-24 20:12:49 +0100

    More discriptive exceptions

--- arca/exceptions.py
+++ arca/exceptions.py
@@ -22,12 +22,24 @@ class BuildError(ArcaException):
         super().__init__(*args, **kwargs)
         self.extra_info = extra_info
 
+    def __str__(self):
+        return "{}\n\n{}".format(
+            super().__str__(),
+            self.extra_info
+        )
+
 
 class PushToRegistryError(ArcaException):
 
     def __init__(self, *args, full_output=None,  **kwargs):
         super().__init__(*args, **kwargs)
         self.full_output = full_output
+
+    def __str__(self):
+        return "{}\n\n{}".format(
+            super().__str__(),
+            self.full_output
+        )
 
 
 class FileOutOfRangeError(ValueError, ArcaException):
@@ -39,3 +51,9 @@ class RequirementsMismatch(ValueError, ArcaException):
     def __init__(self, *args, diff=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.diff = diff
+
+    def __str__(self):
+        return "{}\nDiff:\n{}".format(
+            super().__str__(),
+            self.diff
+        )
