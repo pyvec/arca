c90d522c419a6ce76e61a55398df8539576fb174
Mikuláš Poul <mikulaspoul@gmail.com>
2018-02-25 13:57:18 +0100

    Dynamically listing versions to build base docker images

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import distutils.cmd
+import re
+from pathlib import Path
 
 
 class DeployDockerBasesCommand(distutils.cmd.Command):
@@ -14,6 +16,21 @@ class DeployDockerBasesCommand(distutils.cmd.Command):
     def finalize_options(self):
         pass
 
+    def list_python_versions(self):
+        from arca import Arca
+        arca = Arca()
+
+        _, pyenv = arca.get_files("https://github.com/pyenv/pyenv.git", "master")
+
+        build_for = re.compile(r"^3\.[67]\.[0-9]+$")
+
+        for path in sorted((pyenv / "plugins/python-build/share/python-build/").iterdir()):  # type: Path
+            if path.is_dir():
+                continue
+
+            if build_for.match(path.name):
+                yield path.name
+
     def run(self):
         import requests
         import arca
@@ -27,25 +44,31 @@ class DeployDockerBasesCommand(distutils.cmd.Command):
             params={"page_size": 1000}
         )
         response = response.json()
+        available_tags = [x["name"] for x in response["results"]]
 
-        if arca.__version__ in [x["name"] for x in response["results"]]:
+        if arca.__version__ in available_tags:
             print("This version was already pushed into the registry.")
-            return
-
-        base_arca_name, base_arca_tag = backend.get_arca_base(False)
-
-        print(f"Built image {base_arca_name}:{base_arca_tag}")
-
-        if self.verbose:
-            for x in backend.client.images.push(base_arca_name, tag=base_arca_tag, stream=True):
-                print(x.decode("utf-8"), end="")
+            base_arca_name, base_arca_tag = backend.get_arca_base()
+            print(f"Pulled image {base_arca_name}:{base_arca_tag}, might have to build new python versions.")
         else:
-            backend.client.images.push(base_arca_name, tag=base_arca_tag)
+            base_arca_name, base_arca_tag = backend.get_arca_base(pull=False)
+            print(f"Built image {base_arca_name}:{base_arca_tag}")
 
-        print(f"Pushed image {base_arca_name}:{base_arca_tag}")
+            if self.verbose:
+                for x in backend.client.images.push(base_arca_name, tag=base_arca_tag, stream=True):
+                    print(x.decode("utf-8"), end="")
+            else:
+                backend.client.images.push(base_arca_name, tag=base_arca_tag)
 
-        for docker_base in ["3.6.0", "3.6.1", "3.6.2", "3.6.3", "3.6.4"]:  # TODO: dynamically list these
-            image_name, image_tag = backend.get_python_base(docker_base, pull=False)
+            print(f"Pushed image {base_arca_name}:{base_arca_tag}")
+
+        for python_version in self.list_python_versions():
+            tag = backend.get_python_base_tag(python_version)
+            if tag in available_tags:
+                print(f"Skipping Python version {python_version}, already built for this version of arca.")
+                continue
+
+            image_name, image_tag = backend.get_python_base(python_version, pull=False)
 
             print(f"Built image {image_name}:{image_tag}")
 
