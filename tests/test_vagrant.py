d03bf84c3d0e851f493f29f085aa2aae45d7a8d4
Mikuláš Poul <mikulaspoul@gmail.com>
2018-04-27 21:36:22 +0200

    Updated Vagrant docker to only use one VM and related updates to save on time, bandwidth and space.

--- tests/test_vagrant.py
+++ tests/test_vagrant.py
@@ -1,10 +1,12 @@
 import os
+import shutil
 
 import pytest
+from vagrant import Vagrant
 
 from arca import VagrantBackend, Arca, Task
-from arca.exceptions import ArcaMisconfigured, BuildError
-from common import BASE_DIR, RETURN_COLORAMA_VERSION_FUNCTION
+from arca.exceptions import ArcaMisconfigured
+from common import BASE_DIR, RETURN_COLORAMA_VERSION_FUNCTION, SECOND_RETURN_STR_FUNCTION, TEST_UNICODE
 
 
 def test_validation():
@@ -45,30 +47,61 @@ def test_validation():
         Arca(backend=backend)
 
 
-# `hashicorp/boot2docker` contains docker 1.7 - doesn't work
-# `asdfkljasdf/asdfasdf` doesn't exist
-@pytest.mark.parametrize("box", [None, "hashicorp/boot2docker", "asdfkljasdf/asdfasdf"])
-def test_vagrant(temp_repo_func, box):
+# `hashicorp/boot2docker` could be used to test that the backend fails when the box has old docker (1.7),
+# but it takes a lot of time to download it and to spool it up, it does not make sense to use it.
+# Also a nonexistant box could be tested if it raises an exception.
+# If you want to test that even init of the VM works, set ``destroy`` to True, it will destroy the previous one as well.
+# Set to ``False`` by default to bootup time and bandwidth.
+def test_vagrant(temp_repo_func, destroy=False):
     if os.environ.get("TRAVIS", False):
         pytest.skip("Vagrant doesn't work on Travis")
 
-    kwargs = {}
-    if box:
-        kwargs["box"] = box
-    backend = VagrantBackend(verbosity=2, use_registry_name="docker.io/mikicz/arca-test", **kwargs)
+    backend = VagrantBackend(verbosity=2, use_registry_name="docker.io/mikicz/arca-test",
+                             keep_vm_running=True)
     arca = Arca(backend=backend, base_dir=BASE_DIR)
 
+    if destroy:
+        vagrant_location = backend.get_vm_location()
+        if vagrant_location.exists():
+            vagrant = Vagrant(vagrant_location)
+            vagrant.destroy()
+        shutil.rmtree(vagrant_location)
+
+    # master branch - return colorama version
     temp_repo_func.file_path.write_text(RETURN_COLORAMA_VERSION_FUNCTION)
     requirements_path = temp_repo_func.repo_path / backend.requirements_location
     requirements_path.write_text("colorama==0.3.9")
-
     temp_repo_func.repo.index.add([str(temp_repo_func.file_path), str(requirements_path)])
     temp_repo_func.repo.index.commit("Initial")
 
+    # branch branch - return unicode
+    temp_repo_func.repo.create_head("branch")
+    temp_repo_func.repo.branches.branch.checkout()
+    temp_repo_func.file_path.write_text(SECOND_RETURN_STR_FUNCTION)
+    temp_repo_func.repo.index.add([str(temp_repo_func.file_path)])
+    temp_repo_func.repo.index.commit("Test unicode on a separate branch")
+
     task = Task("test_file:return_str_function")
 
-    if not box:
-        assert arca.run(temp_repo_func.url, temp_repo_func.branch, task).output == "0.3.9"
-    else:
-        with pytest.raises(BuildError):  # fails because of reasons listed above
-            arca.run(temp_repo_func.url, temp_repo_func.branch, task)
+    assert arca.run(temp_repo_func.url, temp_repo_func.branch, task).output == "0.3.9"
+
+    # halt the VM, checks that the VM can be booted when stopped with the vagrant attribute set
+    backend.stop_vm()
+    assert arca.run(temp_repo_func.url, temp_repo_func.branch, task).output == "0.3.9"
+
+    # halt the vm and create a new instance of the backend, to check that the vagrant attribute can be set from existing
+    backend.stop_vm()
+    backend = VagrantBackend(verbosity=2, use_registry_name="docker.io/mikicz/arca-test",
+                             keep_vm_running=True)
+    arca = Arca(backend=backend, base_dir=BASE_DIR)
+    assert arca.run(temp_repo_func.url, temp_repo_func.branch, task).output == "0.3.9"
+
+    # test that two branches can work next to each other
+    assert arca.run(temp_repo_func.url, temp_repo_func.branch, task).output == "0.3.9"
+    assert arca.run(temp_repo_func.url, "branch", task).output == TEST_UNICODE
+
+    backend.stop_vm()
+
+    if destroy:
+        backend.destroy = True
+        backend.stop_vm()
