04ca4655f6d9a304c83b8580404aa89d6ed27842
Petr Viktorin <encukou@gmail.com>
2019-12-09 16:07:52 +0100

    Test with a branch with a slash in its name

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,17 +16,26 @@ def create_temp_repo(file) -> TempRepo:
     git_dir = Path(tempfile.mkdtemp())
     repo = Repo.init(str(git_dir))
 
-    return TempRepo(repo, git_dir, f"file://{git_dir}", "master", git_dir / file)
+    return TempRepo(
+        repo, git_dir, f"file://{git_dir}", "master", git_dir / file,
+    )
 
 
-@pytest.fixture()
-def temp_repo_func():
+@pytest.fixture(params=["master", "branch/with/slash"])
+def temp_repo_func(request):
     temp_repo = create_temp_repo("test_file.py")
 
     temp_repo.file_path.write_text(RETURN_STR_FUNCTION)
 
     temp_repo.repo.index.add([str(temp_repo.file_path)])
     temp_repo.repo.index.commit("Initial")
+
+    branch_name = request.param
+    if branch_name != "master":
+        # Now that there is a commit, create a branch
+        temp_repo = temp_repo._replace(branch=branch_name)
+        branch = temp_repo.repo.create_head(branch_name)
+        temp_repo.repo.head.reference = branch
 
     yield temp_repo
 
